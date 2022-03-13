f7fbd470b1e00e602623d8ad14ad9931c6679785
João Alcântara <joaosantalcantara@gmail.com>
2022-03-13 19:55:09 -0300

    removed section bugged

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
 // import MiddleBlockContent from "../../content/MiddleBlockContent.json";
-import AboutContent from "../../content/AboutContent.json";
+// import AboutContent from "../../content/AboutContent.json";
 // import MissionContent from "../../content/MissionContent.json";
 // import ProductContent from "../../content/ProductContent.json";
 // import ContactContent from "../../content/ContactContent.json";
@@ -36,14 +36,14 @@ const Home = () => {
         icon="about-me.png"
         id="mission"
       /> */}
-      <ContentBlock
+      {/* <ContentBlock
         type="left"
         title={AboutContent.title}
         content={AboutContent.text}
         section={AboutContent.section}
         icon="about-me.png"
         id="about"
-      />
+      /> */}
       {/* <ContentBlock
         type="left"
         title={ProductContent.title}
