8240d33759ca722b22d62f91d6c4011036104300
João Alcântara <joaosantalcantara@gmail.com>
2022-03-13 20:47:31 -0300

    set structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,13 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
-// import MiddleBlockContent from "../../content/MiddleBlockContent.json";
-// import AboutContent from "../../content/AboutContent.json";
-// import MissionContent from "../../content/MissionContent.json";
-// import ProductContent from "../../content/ProductContent.json";
+import MiddleBlockContent from "../../content/MiddleBlockContent.json";
+import AboutContent from "../../content/AboutContent.json";
+import MissionContent from "../../content/MissionContent.json";
+import ProductContent from "../../content/ProductContent.json";
 // import ContactContent from "../../content/ContactContent.json";
 
 // const Contact = lazy(() => import("../../components/ContactForm"));
-// const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
+const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlock = lazy(() => import("../../components/ContentBlock"));
@@ -24,33 +24,33 @@ const Home = () => {
         icon="home-bg.webp"
         id="intro"
       />
-      {/* <MiddleBlock
+      <MiddleBlock
         title={MiddleBlockContent.title}
         content={MiddleBlockContent.text}
         button={MiddleBlockContent.button}
-      /> */}
-      {/* <ContentBlock
+      />
+      <ContentBlock
         type="right"
         title={MissionContent.title}
         content={MissionContent.text}
         icon="about-me.png"
         id="mission"
-      /> */}
-      {/* <ContentBlock
+      />
+      <ContentBlock
         type="left"
         title={AboutContent.title}
         content={AboutContent.text}
         section={AboutContent.section}
         icon="about-me.png"
         id="about"
-      /> */}
-      {/* <ContentBlock
+      />
+      <ContentBlock
         type="left"
         title={ProductContent.title}
         content={ProductContent.text}
         icon="waving.svg"
         id="product"
-      /> */}
+      />
       {/* <Contact
         title={ContactContent.title}
         content={ContactContent.text}
