3373ab9c274961404af59bd9dfe8b27b557db125
João Alcântara <joaosantalcantara@gmail.com>
2022-03-14 16:29:53 -0300

    hotfix home section

--- src/components/ContentBlock/RightContentBlock/index.tsx
+++ src/components/ContentBlock/RightContentBlock/index.tsx
@@ -9,7 +9,7 @@ import {
   Content,
   ContentWrapper,
   ButtonWrapper,
-  Ellipse
+  Ellipse,
 } from "./styles";
 
 const RightBlock = ({
@@ -29,32 +29,33 @@ const RightBlock = ({
   return (
     <RightBlockContainer>
       {/* <Fade direction="right"> */}
-        <Row justify="space-between" align="middle" id={id}>
-          <Col lg={11} md={11} sm={11} xs={24}>
-            <ContentWrapper>
-              <h6>{t(title)}</h6>
-              <Content>{t(content)}</Content>
-              <ButtonWrapper>
-                {typeof button === "object" &&
-                  button.map((item: any, id: number) => {
-                    return (
-                      <Button
-                        key={id}
-                        color={item.color}
-                        fixedWidth={true}
-                        onClick={() => scrollTo("about")}
-                      >
-                        {t(item.title)}
-                      </Button>
-                    );
-                  })}
-              </ButtonWrapper>
-            </ContentWrapper>
-          </Col>
-          <Col lg={13} md={11} sm={12} xs={0}>
-            <SvgIcon src={icon} width="100%" height="100%" />
-          </Col>
-        </Row>
+      <Row justify="space-between" align="middle" id={id}>
+        <Col lg={12} md={11} sm={11} xs={24}>
+          <ContentWrapper>
+            <h6>{t(title)}<span>.</span></h6>
+            <Content>Todos os conteúdos, a ordem e a forma <span>que você deve estudar os conteúdos do ENEM que me levaram a aprovação em Medicina na Federal as 18 anos.</span></Content>
+            {/* <Content>{t(content)}</Content> */}
+            <ButtonWrapper>
+              {typeof button === "object" &&
+                button.map((item: any, id: number) => {
+                  return (
+                    <Button
+                      key={id}
+                      color={item.color}
+                      fixedWidth={true}
+                      onClick={() => scrollTo("about")}
+                    >
+                      {t(item.title)}
+                    </Button>
+                  );
+                })}
+            </ButtonWrapper>
+          </ContentWrapper>
+        </Col>
+        <Col lg={12} md={11} sm={12} xs={0}>
+          <SvgIcon src={icon} width="90%" height="auto" />
+        </Col>
+      </Row>
       {/* </Fade> */}
       <Ellipse />
     </RightBlockContainer>
